82add7f8d7a95cfa9ac38298fc7ca9d307668c7d
lokraj thapa <lokrajthapa741@gmail.com>
2024-10-26 15:48:08 +0545

    Table heading changed

--- resources/views/attendees/index.blade.php
+++ resources/views/attendees/index.blade.php
@@ -24,7 +24,7 @@
                         <tr>
                             <th scope="col" class="px-6 py-3">Name</th>
                             <th scope="col" class="px-6 py-3">Email</th>
-                            <th scope="col" class="px-6 py-3">Event ID</th>
+                            <th scope="col" class="px-6 py-3">Event </th>
                             <th scope="col" class="px-6 py-3">Action</th>
                         </tr>
                     </thead>
@@ -36,7 +36,7 @@
                                         {{ $attendee->name }}
                                     </td>
                                     <td class="px-6 py-4">{{ $attendee->email }}</td>
-                                    <td class="px-6 py-4">{{ $attendee->event_id }}</td>
+                                    <td class="px-6 py-4">{{ $attendee->event->title }}</td>
                                     <td class="flex">
                                         <a href="{{ route('attendees.show', $attendee->id) }}" class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800 m-2">Show</a>
                                         <a href="{{ route('attendees.edit', $attendee->id) }}" class="focus:outline-none text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900 m-2">Edit</a>
--- resources/views/events/index.blade.php
+++ resources/views/events/index.blade.php
@@ -26,7 +26,7 @@
                             <th scope="col" class="px-6 py-3">Description</th>
                             <th scope="col" class="px-6 py-3">Date</th>
                             <th scope="col" class="px-6 py-3">Location</th>
-                            <th scope="col" class="px-6 py-3">Category ID</th>
+                            <th scope="col" class="px-6 py-3">Category </th>
                             <th scope="col" class="px-6 py-3">Action</th>
                         </tr>
                     </thead>
@@ -40,7 +40,7 @@
                                     <td class="px-6 py-4">{{ $event->description }}</td>
                                     <td class="px-6 py-4">{{ $event->date }}</td>
                                     <td class="px-6 py-4">{{ $event->location }}</td>
-                                    <td class="px-6 py-4">{{ $event->category_id }}</td>
+                                    <td class="px-6 py-4">{{ $event->category->name }}</td>
                                     <td class="flex">
                                         <a href="{{ route('events.show', $event->id) }}" class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800 m-2">Show</a>
                                         <a href="{{ route('events.edit', $event->id) }}" class="focus:outline-none text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900 m-2">Edit</a>
